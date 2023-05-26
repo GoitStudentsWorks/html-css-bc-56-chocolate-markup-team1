66e5ce756d6845006b55ba406a7dad9c23824be1
DenisPitsul <denia.pitsul@gmail.com>
2023-05-26 16:10:12 +0300

    added is-hidden to modal

--- src/js/new-chocolate-modal.js
+++ src/js/new-chocolate-modal.js
@@ -1,11 +1,11 @@
 (() => {
     const refs = {
-    //   openModalBtn: document.querySelector("[data-new-chocolate-modal-open]"),
+      openModalBtn: document.querySelector("[data-new-chocolate-modal-open]"),
       closeModalBtn: document.querySelector("[data-new-chocolate-modal-close]"),
       modal: document.querySelector("[data-new-chocolate-modal]"),
     };
   
-    // refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
