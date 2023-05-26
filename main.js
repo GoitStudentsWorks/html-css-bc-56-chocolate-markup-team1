cb68416c235005b4a3ba6ad2d38f08793f9ebef1
DenisPitsul <denia.pitsul@gmail.com>
2023-05-26 16:42:27 +0300

    added close button hover

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './src/js/mobile.menu.js';
-import './src/js/buy-now-modal.js';
-import './src/js/leave-review-modal.js';
-import './src/js/new-chocolate-modal.js';
+import './js/mobile.menu.js';
+import './js/buy-now-modal.js';
+import './js/leave-review-modal.js';
+import './js/new-chocolate-modal.js';
